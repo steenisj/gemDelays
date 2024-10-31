ee1f40288b4c54f1f4f6ed6d9bd3112d75c30d1c
Jacob Steenis <jahejahe06@gmail.com>
2024-10-31 16:28:33 +0100

    first draft of awkward arrays usage on pre-filtering the data -- checks still need to be made

--- delay_tdr_uproot.py
+++ delay_tdr_uproot.py
@@ -43,7 +43,9 @@ filelist = find_root_files("/eos/cms/store/group/dpg_gem/comm_gem/P5_Commissioni
 tree_name = "muNtupleProducer/MuDPGTree"
 filelist = [x+":"+tree_name for x in filelist]
 
-variables = ["gemPadDigiCluster_layer", "gemPadDigiCluster_station", "gemPadDigiCluster_region", "gemPadDigiCluster_ClusterFirstPad", "gemPadDigiCluster_PadBX", "gemPadDigiCluster_PadClusterSize", "gemPadDigiCluster_etaPartition", "gemPadDigiCluster_chamber", "gemPadDigiCluster_ClusterALCTMatchTime", "mu_propagated_isME11", "mu_propagated_station", "mu_propagated_region", "mu_propagated_Outermost_z", "mu_propagated_etaP", "mu_propagated_pt", "mu_propagated_chamber", "mu_propagated_strip", "mu_propagated_layer"]
+cluster_variables = ["gemPadDigiCluster_layer", "gemPadDigiCluster_station", "gemPadDigiCluster_region", "gemPadDigiCluster_ClusterFirstPad", "gemPadDigiCluster_PadBX", "gemPadDigiCluster_PadClusterSize", "gemPadDigiCluster_etaPartition", "gemPadDigiCluster_chamber", "gemPadDigiCluster_ClusterALCTMatchTime"]
+muon_variables = ["mu_propagated_isME11", "mu_propagated_station", "mu_propagated_region", "mu_propagated_Outermost_z", "mu_propagated_etaP", "mu_propagated_pt", "mu_propagated_chamber", "mu_propagated_strip", "mu_propagated_layer"]
+variables = cluster_variables + muon_variables
 
 output_path = f"results/GEM_delay_data.root"
 
@@ -100,15 +102,126 @@ for station in stations:
 # Loop over chunks of data using uproot.iterate
 counter = 0
 counter_limit = None #10000
-for data in uproot.iterate(filelist, variables, library='ak', step_size=1):     
+for data in uproot.iterate(filelist, variables, library='ak', step_size=100000):     
     if counter_limit!=None:
         if counter>counter_limit:
             break
     counter+=1
+    print("Working on event: ", counter*100000)
 
-    if (counter%1000==0):
-        print("Working on event: ", counter)
 
+    muon_data = data[muon_variables]
+    cluster_data = data[cluster_variables]
+    #Check to make sure that we've got some sort of ME11 hit in the events
+    ME11_mask = ak.any(muon_data['mu_propagated_isME11'], axis=-1)
+    muon_data = muon_data[ME11_mask]
+    cluster_data = cluster_data[ME11_mask]
+
+    muon_location_mask = (muon_data['mu_propagated_isME11']) & (muon_data['mu_propagated_station']==1) & (muon_data['mu_propagated_region']*muon_data['mu_propagated_Outermost_z']>0) & (muon_data['mu_propagated_etaP']>=1) & (muon_data['mu_propagated_etaP']<=8) & (muon_data['mu_propagated_pt']>10)
+    muon_data = muon_data[muon_location_mask]
+
+    #print(len(muon_data))
+    #print(len(cluster_data))
+    num_mask = ak.num(muon_data['mu_propagated_isME11'])>0
+    #print(len(num_mask))
+    muon_data = muon_data[num_mask]
+    cluster_data = cluster_data[num_mask]
+
+    #print(muon_data['mu_propagated_chamber'].tolist()[0])
+    #print(cluster_data['gemPadDigiCluster_chamber'].tolist()[0])
+
+
+    #for muon_field in muon_data.fields:
+    #    for cluster_field in cluster_fields:
+    #       conbinations
+
+    chamber_combinations = ak.cartesian({"muon_chamber": muon_data['mu_propagated_chamber'], "cluster_chamber": cluster_data['gemPadDigiCluster_chamber']})
+    layer_combinations = ak.cartesian({"muon_layer": muon_data['mu_propagated_layer'], "cluster_layer": cluster_data['gemPadDigiCluster_layer']})
+    station_combinations = ak.cartesian({"muon_station": muon_data['mu_propagated_station'], "cluster_station": cluster_data['gemPadDigiCluster_station']})
+    region_combinations = ak.cartesian({"muon_region": muon_data['mu_propagated_region'], "cluster_region": cluster_data['gemPadDigiCluster_region']})
+
+    eta_combinations = ak.cartesian({"muon_eta": muon_data['mu_propagated_etaP'], "cluster_eta": cluster_data['gemPadDigiCluster_etaPartition']})
+    proximity_combinations = ak.cartesian({"muon_prox": muon_data['mu_propagated_strip'], "cluster_prox": cluster_data['gemPadDigiCluster_ClusterFirstPad']})
+
+    location_mask = (chamber_combinations['muon_chamber'] == chamber_combinations['cluster_chamber']) & (layer_combinations['muon_layer'] == layer_combinations['cluster_layer']) & (station_combinations['muon_station'] == station_combinations['cluster_station']) & (region_combinations['muon_region'] == region_combinations['cluster_region']) & (eta_combinations['muon_eta'] == eta_combinations['cluster_eta'])
+
+    proximity_combinations['muon_prox'] = np.floor(proximity_combinations['muon_prox']/2.0) #To convert between strip and pad
+    proximity_mask = (abs(proximity_combinations['cluster_prox'] - proximity_combinations['muon_prox']) <= 5)
+
+    #Preparing the actual data
+    BX_combinations = ak.cartesian({"muon_chamber": muon_data['mu_propagated_chamber'], "cluster_PadBX": cluster_data['gemPadDigiCluster_PadBX']})
+    ALC_combinations = ak.cartesian({"muon_chamber": muon_data['mu_propagated_chamber'], "cluster_ALC": cluster_data['gemPadDigiCluster_ClusterALCTMatchTime']})
+    size_combinations = ak.cartesian({"muon_chamber": muon_data['mu_propagated_chamber'], "cluster_size": cluster_data['gemPadDigiCluster_PadClusterSize']})
+    firstPad_combinations = ak.cartesian({"muon_chamber": muon_data['mu_propagated_chamber'], "cluster_firstPad": cluster_data['gemPadDigiCluster_ClusterFirstPad']})
+
+    BX_combinations = BX_combinations[location_mask & proximity_mask]
+    ALC_combinations = ALC_combinations[location_mask & proximity_mask]
+    size_combinations = size_combinations[location_mask & proximity_mask]
+    eta_combinations = eta_combinations[location_mask & proximity_mask]
+    firstPad_combinations = firstPad_combinations[location_mask & proximity_mask]
+
+    gemBX = ak.Array([shiftingBX(gemPadDigiCluster_PadBX, CSCConstants_LCT_CENTRAL_BX, tmbL1aWindowSize, gemPadDigiCluster_ClusterALCTMatchTime) for gemPadDigiCluster_PadBX, gemPadDigiCluster_ClusterALCTMatchTime in zip(ak.flatten(BX_combinations['cluster_PadBX'],axis=None), ak.flatten(ALC_combinations['cluster_ALC'],axis=None))])
+    expandedClusters = ak.Array([clusterIdExpander(firstId, numPads, etaPartition) for firstId, numPads, etaPartition in zip(ak.flatten(firstPad_combinations,axis=None), ak.flatten(size_combinations['cluster_size'],axis=None), ak.flatten(eta_combinations['cluster_eta']))])
+    expandedBX = ak.Array([clusterBXExpander(firstId, numPads) for firstId, numPads in zip(gemBX, ak.flatten(size_combinations['cluster_size'],axis=None))])
+
+    chamber_combinations = chamber_combinations[location_mask & proximity_mask]
+    layer_combinations = layer_combinations[location_mask & proximity_mask]
+    station_combinations = station_combinations[location_mask & proximity_mask]
+    region_combinations = region_combinations[location_mask & proximity_mask]
+
+    for station in stations:
+        for layer in layers:
+            for region in regions:
+                for chamber in chambers:
+                    #To prevent naming issues when accessing via root
+                    region_label = str(region)
+                    if region<0:
+                        #region_label = str(region).replace("-","neg")
+                        region_label = str(region).replace("-1","M")
+                    elif region>0:
+                        region_label = str(region).replace("1","P")
+
+                    station_mask = station_combinations = station
+                    layer_mask = station_combinations = layer
+                    region_mask = station_combinations = region
+                    chamber_mask = station_combinations = chamber
+                    location_mask = station_mask & layer_mask & chamber_mask & region_mask
+
+                    hist_expandedClusters = expandedClusters[location_mask]
+                    hist_expandedBX = expandedBX[location_mask]
+
+                    # Fill the histogram using FillN()
+                    n_events = len(ak.flatten(hist_expandedClusters, axis=None))
+                    x_data = ak.flatten(hist_expandedClusters, axis=None)
+                    y_data = ak.flatten(hist_expandedBX, axis=None)
+                    weights = ak.ones_like(ak.flatten(hist_expandedBX, axis=None))
+
+                    if n_events>0:
+                        name = f"GE{station}1_{region_label}_{chamber}_L{layer}"
+                        chamber_hists[name]['data'].FillN(n_events, array('d', x_data), array('d', y_data), array('d', weights))
+
+
+for chamber in chamber_hists.keys():
+    chamber_hists[chamber]['data'].SaveAs("results/delay_plots/"+str(chamber)+".root")
+
+
+#    print(mod_region_combinations)
+
+    # Fill the histogram using FillN()
+    '''n_events = len(ak.flatten(expandedClusters, axis=None))
+    x_data = ak.flatten(expandedClusters, axis=None)
+    y_data = ak.flatten(expandedBX, axis=None)
+    weights = ak.ones_like(ak.flatten(expandedBX, axis=None))
+
+    if n_events>0:
+        if muon_region == -1:
+            muon_region = "M"
+        elif muon_region == 1:
+            muon_region = "P"
+        name = f"GE{muon_station}1_{muon_region}_{muon_chamber}_L{muon_layer}"
+        chamber_hists[name]['data'].FillN(n_events, array('d', x_data), array('d', y_data), array('d', weights))
+
+    #print(combinations['muon_chamber']==combinations['cluster_station'])
     matchedChamber = []
     for muon in range(len(data['mu_propagated_isME11'])):
         
@@ -160,4 +273,4 @@ for data in uproot.iterate(filelist, variables, library='ak', step_size=1):
 for chamber in chamber_hists.keys():
     chamber_hists[chamber]['data'].SaveAs("results/delay_plots/"+str(chamber)+".root")
 
-
+'''
